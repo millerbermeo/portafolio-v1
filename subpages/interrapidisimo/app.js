562f0ca9ed923d0535483e39374564619c1c881a
millerbermeo <millerbermeo30@gmail.com>
2023-04-11 23:05:47 -0500

    portafolio v12

---
+++ subpages/interrapidisimo/app.js
@@ -0,0 +1,27 @@
+window.onload=inicio;
+
+function inicio() {
+    let btn = document.querySelector('.logos');
+    let nav = document.querySelector('.nav-menu');
+    let menu = document.querySelector('.logo')
+    let menu2 = document.querySelector('.logo2')
+
+    btn.addEventListener('click', ()=> {
+        if(nav.classList.contains('noactive')) {
+            nav.classList.add('active')
+            nav.classList.remove('noactive')
+            
+            menu.classList.add('quitar')
+            menu2.classList.add('poner')
+            
+        } else {
+            nav.classList.remove('active')
+            nav.classList.add('noactive')
+
+            menu2.classList.remove('poner')
+            menu.classList.remove('quitar')
+        }
+    })
+
+
+}
